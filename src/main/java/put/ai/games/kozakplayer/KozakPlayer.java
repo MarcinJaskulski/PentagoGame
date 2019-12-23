18aec5fdfaab156772e9f351bf833fd928b93904
Marcin Jaskulski <jaskulski.marcin@outlook.com>
2019-12-23 23:31:42 +0100

    17,5 pkt

--- src/main/java/put/ai/games/kozakplayer/KozakPlayer.java
+++ src/main/java/put/ai/games/kozakplayer/KozakPlayer.java
@@ -37,17 +37,28 @@ public class KozakPlayer extends Player {
     	Move m;
     	//Jeśli mogę wygrać to zrobię to!
     	m = canWin(b);
-    	if(m != null) {
-    		info++; 
+    	if(m != null)
     		return m;
-    	}
+    	
+    	m = winBetween(b, getColor(), opponentColor);
+    	if(m != null)
+    		return m;
     	
     	//Jeśli przeciwnik może wygrać to przeszkodzę mu!
     	m = opponentCanWin(b);
-    	if(m != null) {
-    		info++; 
+    	if(m != null)
     		return m;
-    	}
+    	m = winBetween(b, opponentColor, getColor());
+    	if(m != null)
+    		return m;
+    	
+    	
+    	
+    	//Startegia ruchu
+    	m = centralSquare(b);
+    	if(m != null)
+    		return m;
+    	
     	
     	
     	
@@ -117,6 +128,76 @@ public class KozakPlayer extends Player {
 		return null;
     }
     
+    // Musi zaczynać i kończyć się na swój znak + jeden biały znak pomiędzy
+    private Move winBetween(PentagoBoard b, Color lookingForColor, Color badColor) {
+		int poz = 0;
+		int pozEmpty=0;
+		int pion = 0;
+		int pionEmpty=0;
+		
+		for(int i=0; i<b.getSize(); i++) {
+			poz=0;
+			pozEmpty=0;
+			pion=0;
+			pionEmpty=0;
+			
+			for(int j=0; j<b.getSize(); j++) {
+				if(b.getState(j, i) == lookingForColor) // zaczynajacy
+					poz++;
+				else if(b.getState(j, i) == Color.EMPTY && poz>0) // jesli jest pusty, ale cos juz zalapalismy
+					if(pozEmpty==0)
+						pozEmpty=1;
+					else {
+						poz=0;
+						pozEmpty=0;
+					}
+				else if(b.getState(j, i) == badColor){ 
+					poz = 0;
+					pozEmpty=0;
+				}
+				
+				if(b.getState(i, j) == lookingForColor)
+					pion++;
+				else if(b.getState(i, j) == Color.EMPTY && pion>0)
+					if(pionEmpty==0)
+						pionEmpty=1;
+					else {
+						pion=0;
+						pionEmpty=0;
+					}
+				else if(b.getState(i, j) == badColor) {
+					pion=0;
+					pionEmpty=0;
+				}
+				
+				//poszukiwanie wolnego miejsca
+				if(poz + pozEmpty > 4) {
+					for(int k=j-4; k<=j; k++) {
+						if(b.getState(k, i) == Color.EMPTY)
+							return new PentagoMove(k,i,diagonalRotate(k,i)[0],
+													   diagonalRotate(k,i)[1],
+													   diagonalRotate(k,i)[2],
+													   diagonalRotate(k,i)[3],
+													   getColor());
+					}
+				}
+				if(pion + pionEmpty > 4) {
+					for(int k=j-4; k<=j; k++) {
+						if(b.getState(i, k) == Color.EMPTY)
+							return new PentagoMove(i,k,diagonalRotate(i,k)[0],
+													   diagonalRotate(i,k)[1],
+													   diagonalRotate(i,k)[2],
+													   diagonalRotate(i,k)[3],
+													   getColor());
+					}
+				}
+				
+				
+			}
+		}
+		return null;
+    }
+    
     private Move opponentCanWin(PentagoBoard b) {
 		int poz = 0;
 		int pion = 0;
@@ -135,29 +216,35 @@ public class KozakPlayer extends Player {
 				else
 					pion=0;
 				
-				if(poz == 4) {
-					//co obrocic -> ta gdzie jest czasc zwycieskiej lini
-					int[] diagonalRotate = thisSquareRotate(j,i);
-					int[] rotateFrom = {diagonalRotate[0], diagonalRotate[1]}; //x,y
-					int[] roatteTo  = {diagonalRotate[2], diagonalRotate[3]}; //x,y   						
-					
+				if(poz == 4) { //dziala, ale uzaleznic jakos rotacje sensowniej!!!!									
 					// czy moge ustwaic na koncu, jesli nie to  sprobuje na poczatku
 					if(j+1!=b.getSize() && b.getState(j+1, i) == Color.EMPTY) 
-						return new PentagoMove(j+1,i,rotateFrom[0],rotateFrom[1],roatteTo[0],roatteTo[1],getColor());
+						return new PentagoMove(j+1,i, thisSquareRotate(j+1,1)[0],
+													 thisSquareRotate(j+1,1)[1],
+													 thisSquareRotate(j+1,1)[2],
+													 thisSquareRotate(j+1,1)[3],
+													 getColor());
 					else if(j-4>=0 && b.getState(j-4, i) == Color.EMPTY) 
-						return new PentagoMove(j-4,i,rotateFrom[0],rotateFrom[1],roatteTo[0],roatteTo[1],getColor());
+						return new PentagoMove(j-4,i,thisSquareRotate(j-4,1)[0],
+													 thisSquareRotate(j-4,1)[1],
+													 thisSquareRotate(j-4,1)[2],
+													 thisSquareRotate(j-4,1)[3],
+													 getColor());
 				}
 				if(pion == 4) {
-					//co obrocic -> ta gdzie jest czasc zwycieskiej lini
-					int[] diagonalRotate = thisSquareRotate(i,j);
-					int[] rotateFrom = {diagonalRotate[0], diagonalRotate[1]}; //x,y
-					int[] roatteTo  = {diagonalRotate[2], diagonalRotate[3]}; //x,y   						
-					
 					// czy moge ustawic na dole, jesli nie to sprobuje na gorze
 					if(j+1!=b.getSize() && b.getState(i, j+1) == Color.EMPTY) 
-						return new PentagoMove(i,j+1,rotateFrom[0],rotateFrom[1],roatteTo[0],roatteTo[1],getColor());
+						return new PentagoMove(i,j+1,thisSquareRotate(i,j+1)[0],
+													 thisSquareRotate(i,j+1)[1],
+													 thisSquareRotate(i,j+1)[2],
+													 thisSquareRotate(i,j+1)[3],
+													 getColor());
 					else if(j-4>=0 && b.getState(i, j-4) == Color.EMPTY) 
-						return new PentagoMove(i,i-4,rotateFrom[0],rotateFrom[1],roatteTo[0],roatteTo[1],getColor());
+						return new PentagoMove(i,j-4,thisSquareRotate(j,j-4)[0],
+													 thisSquareRotate(j,j-4)[1],
+													 thisSquareRotate(j,j-4)[2],
+													 thisSquareRotate(j,j-4)[3],
+													 getColor());
 				}	
 			}
 		}
@@ -236,5 +323,20 @@ public class KozakPlayer extends Player {
 		int[] tab = {rotateFrom[0], rotateFrom[1], roatteTo[0], roatteTo[1]};
 		return tab;
     }
+    
+    
+    private Move centralSquare(PentagoBoard b) {
+    	if(b.getState(1, 1) == Color.EMPTY)
+    		return new PentagoMove(1,1,0,0,2,0,getColor());
+    	if(b.getState(1, 4) == Color.EMPTY)
+    		return new PentagoMove(1,4,0,3,2,3,getColor());
+    	if(b.getState(4, 1) == Color.EMPTY)
+    		return new PentagoMove(4,1,3,0,5,0,getColor());
+    	if(b.getState(4, 4) == Color.EMPTY)
+    		return new PentagoMove(4,4,3,3,5,3,getColor());
+    	
+    	return null;
+    }
+    
     
 }
